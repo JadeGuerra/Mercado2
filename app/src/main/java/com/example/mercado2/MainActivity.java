5d67309429e6ccc9e432b9547a66d4cadafaa655
jade <jug.guerra@gmail.com>
2020-10-18 16:40:50 -0300

    First

---
+++ app/src/main/java/com/example/mercado2/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.mercado2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goNovo(View view) {
+        Intent intent = new Intent(this, iniciarCompra.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/mercado2/iniciarCompra.java
@@ -0,0 +1,13 @@
+package com.example.mercado2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class iniciarCompra extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar_compra);
+    }
+}
